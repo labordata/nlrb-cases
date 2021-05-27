df9ea684729134cba2f885cdbba6fb144cb9087e
Forest Gregg <fgregg@uchicago.edu>
2021-05-27 09:11:15 -0400

    allegations

--- nlrb/__init__.py
+++ nlrb/__init__.py
@@ -1,8 +1,12 @@
+import typing
+import time
+import urllib.parse
+import functools
+import datetime
+
 import scrapelib
 import lxml.html
 import requests
-import typing
-import csv
 
 
 CaseTypes = typing.Sequence[typing.Literal['C', 'R']]
@@ -12,65 +16,40 @@ Statuses = typing.Sequence[typing.Literal['Open', 'Closed', 'Open - Blocked']]
 class NLRB(scrapelib.Scraper):
     base_url = 'https://www.nlrb.gov'
 
-    def csv_search(self,
-                   case_types: typing.Optional[CaseTypes] = None,
-                   statuses: typing.Optional[Statuses] = None,
-                   date_start: typing.Optional['datetime.date'] = None,
-                   date_end: typing.Optional['datetime.date'] = None):
+    def _download_link(self,
+                       search_url: str,
+                       case_number_field: str,
+                       case_types: typing.Optional[CaseTypes] = None,
+                       statuses: typing.Optional[Statuses] = None,
+                       date_start: typing.Optional['datetime.date'] = None,
+                       date_end: typing.Optional['datetime.date'] = None):
 
         params = self._prepare_search_params(case_types,
                                              statuses,
                                              date_start,
                                              date_end)
 
-        response = requests.get(self.base_url + '/search/case', params=params)
+        response = requests.get(search_url, params=params)
 
         page = lxml.html.fromstring(response.text)
-        page.make_links_absolute(self.base_url + '/search/case')
+        page.make_links_absolute(search_url)
 
         download_link, = page.xpath("//a[@id='download-button']")
 
         payload = {'cacheId': download_link.get('data-cacheid'),
                    'typeOfReport': download_link.get('data-typeofreport'),
-                   'token': ''}
+                   'token': str(datetime.datetime.now())}
 
         response = self.post(self.base_url + '/nlrb-downloads/start-download',
                              data=payload)
 
         result = response.json()['data']
         while not result['finished']:
-            response = self.get(self.base_url + '/nlrb-downloads/progress/' + result['id'])
+            time.sleep(1)
+            response = requests.get(self.base_url + '/nlrb-downloads/progress/' + str(result['id']))
             result = response.json()['data']
 
-        response = self.get(self.base_url + result['filename'], stream=True)
-        lines = (line.decode('utf-8') for line in response.iter_lines())
-        reader = csv.DictReader(lines)
-
-        for row in reader:
-            row['case type'] = self._case_type(row['Case Number'])
-
-            yield row
-
-    def web_search(self,
-                   case_types: typing.Optional[CaseTypes] = None,
-                   statuses: typing.Optional[Statuses] = None,
-                   date_start: typing.Optional['datetime.date'] = None,
-                   date_end: typing.Optional['datetime.date'] = None):
-
-        params = self._prepare_search_params(case_types,
-                                             statuses,
-                                             date_start,
-                                             date_end)
-
-        response = requests.get(self.base_url + '/search/case', params=params)
-
-        yield from self._parse_search_results(response)
-
-        for response in self._paginate(response):
-            yield from self._parse_search_results(response)
-
-
-
+        return self.base_url + result['filename']
 
     def _prepare_search_params(self,
                                case_types: typing.Optional[CaseTypes] = None,
@@ -99,39 +78,6 @@ class NLRB(scrapelib.Scraper):
 
         return params
 
-    def _parse_search_results(self, response: 'requests.models.Response'):
-
-        page = lxml.html.fromstring(response.text)
-        page.make_links_absolute(self.base_url + '/search/case')
-
-        for result in page.xpath("//div[@class='wrapper-div']"):
-            result_dict = {}
-
-            name, = result.xpath("./div[@class='type-div']//a/text()")
-            result_dict['name'] = name.strip()
-
-            left_column = result.xpath(".//div[@class='left-div']/strong")
-            right_column = result.xpath(".//div[@class='right-div']/strong")
-            columns = left_column + right_column
-
-            for header_element in columns:
-                header = header_element.text.strip(': ')
-                if header == 'Case Number':
-                    link = header_element.getnext()
-                    case_number = link.text.strip()
-                    result_dict[header] = case_number
-                    result_dict['url'] = link.get('href')
-
-                    result_dict['case type'] = self._case_type(case_number)
-
-                elif header == 'Date Filed':
-                    date_str = header_element.tail
-                    result_dict[header] = datetime.datetime.strptime(date_str, '%B %d, %Y').date()
-                else:
-                    result_dict[header] = header_element.tail
-
-            yield result_dict
-
     def _case_type(self, case_number: str) -> str:
         if '-RC-' in case_number:
             case_type = 'RC'
@@ -144,7 +90,7 @@ class NLRB(scrapelib.Scraper):
         elif '-UC-' in case_number:
             case_type = 'UC'
         elif '-CA-' in case_number:
-            case_type = 'CA' # what's this?
+            case_type = 'CA'  # what's this?
         elif '-CD-' in case_number:
             case_type = 'CD'
         elif '-CC-' in case_number:
@@ -155,28 +101,22 @@ class NLRB(scrapelib.Scraper):
             case_type = 'CB'
         elif '-AC-' in case_number:
             case_type = 'AC'
+        elif '-WH-' in case_number:
+            case_type = 'WH'
         else:
             print(case_number)
             raise
 
         return case_type
 
-    def _paginate(self, response: 'requests.models.Response'):
+    filings = functools.partialmethod(_download_link,
+                                      search_url=base_url + '/reports/graphs-data/recent-filings',
+                                      case_number_field='Case Number')
 
-        while True:
-            page = lxml.html.fromstring(response.text)
-            page.make_links_absolute(self.base_url + '/search/case')
-
-            next_page_links = page.xpath("//a[@rel='next']")
-            if not next_page_links:
-                break
-
-            next_page_link, = next_page_links
-
-            response = self.get(next_page_link.get('href'))
-            print(next_page_link.get('href'))
-
-            yield response
+    tallies = functools.partialmethod(_download_link,
+                                      search_url=base_url + '/reports/graphs-data/recent-election-results',
+                                      case_types=None,
+                                      case_number_field='Case')
 
     def case_details(self, case_number: str):
         case_url = self.base_url + '/case/' + case_number
@@ -201,7 +141,7 @@ class NLRB(scrapelib.Scraper):
             if header == 'Case Number':
                 case_number = header_element.tail.strip()
                 details[header] = case_number
-                details['case type'] = self._case_type(case_number)
+                details['case_type'] = self._case_type(case_number)
 
             elif header == 'Date Filed':
                 date_str = header_element.getnext().text.strip()
@@ -210,9 +150,10 @@ class NLRB(scrapelib.Scraper):
                 details[header] = header_element.tail.strip()
 
         # Docket
-        docket = []
         if 'Docket Activity data is not available' not in response.text:
-            details['docket'] = self._docket(page, case_url)
+            details['docket'] = self._docket(page, case_number)
+        else:
+            details['docket'] = []
 
         related_documents = []
         if 'Related Documents data is not available' not in response.text:
@@ -221,11 +162,16 @@ class NLRB(scrapelib.Scraper):
             for doc_link in document_list.xpath('.//a'):
                 related_documents.append({'name': doc_link.text,
                                           'url': doc_link.get('href')})
-        details['related documents'] = related_documents
+        details['related_documents'] = related_documents
 
+        allegations = []
         if 'Allegations data is not available' not in response.text:
-            print(case_number)
-            raise
+            allegation_header, = page.xpath(".//h2[text()='Allegations']")
+            allegation_list = allegation_header.getnext().getnext()
+            for item in allegation_list.xpath('.//li'):
+                allegations.append({'allegation': item.text})
+
+        details['allegations'] = allegations
 
         # Participants
         participants = []
@@ -241,7 +187,7 @@ class NLRB(scrapelib.Scraper):
                 participant_entry['type'], *participant_text = participant_text
                 participant_entry['participant'] = '\n'.join(participant_text).strip()
                 participant_entry['address'] = '\n'.join(line.strip() for line in address.xpath('./text()')).strip()
-                participant_entry['phone number'] = phone.text.strip()
+                participant_entry['phone_number'] = phone.text.strip()
 
                 participants.append(participant_entry)
 
@@ -250,104 +196,59 @@ class NLRB(scrapelib.Scraper):
         # Related Cases
         details['related cases'] = page.xpath("//table[starts-with(@class, 'related-case')]/tbody//a/text()")
 
-        return details
+        return {k.lower().replace(' ', '_'): v for k, v in details.items()}
 
-    def _docket(self, page, case_url):
-        # not currently working on https://www.nlrb.gov/case/14-RC-012769?page=1
-        docket = []
-        
-        while True:
-            docket_table, = page.xpath("//div[@id='case_docket_activity_data']/table/tbody")
-            for row in docket_table.xpath('./tr'):
-                docket_entry = {}
+    def _docket(self, page, case_number):
 
-                date, document, party = row.xpath('./td')
-                docket_entry['date'] = datetime\
-                    .datetime\
-                    .strptime(date.text.strip(),
-                              '%m/%d/%Y').date()
-                if document:
-                    document_link, = document.xpath('./a')
-                    docket_entry['document'] = document_link.text.strip()
-                    docket_entry['url'] = document_link.get('href')
-                else:
-                    docket_entry['document'] = document.text.strip()
+        docket_table, = page.xpath("//div[@id='case_docket_activity_data']/table/tbody")
+        docket = list(self._parse_docket_table(docket_table))
 
-                docket_entry['issued by/filed by'] = party.text.strip()
+        last_page_links = page.xpath("//a[@title='Go to last page']")
+        if last_page_links:
 
-                docket.append(docket_entry)
-            
-            next_page_links = page.xpath("//a[@rel='next']")
-            if not next_page_links:
-                break
+            last_page_link, = last_page_links
+            last_page = urllib.parse.parse_qs(urllib.parse.urlparse(last_page_link.get('href')).query)['page'][0]
 
-            next_page_link, = next_page_links
+            for page_number in range(1, int(last_page) + 1):
 
-            print(next_page_link.get('href'))
-            response = self.get(next_page_link.get('href'))
-            
-            page = lxml.html.fromstring(response.text)
-            page.make_links_absolute(case_url)
+                next_page_url = self.base_url + '/sort-case-decisions-cp/{}/ds_activity%20desc/case-docket-activity/ds-activity-date'.format(case_number)
+
+                response = self.get(next_page_url,
+                                    params={'page': page_number})
+
+                page_snippet = lxml.html.fromstring(response.json()[3]['data'])
+                docket_table, = page_snippet.xpath("//table/tbody")
+
+                docket += list(self._parse_docket_table(docket_table))
 
         return docket
 
+    def _parse_docket_table(self, docket_table: 'lxml.html.HmlElement'):
+        for row in docket_table.xpath('./tr'):
+            docket_entry: typing.Dict[str, typing.Union[str, 'datetime.date', None]] = {}
 
-    def certifications(self,
-                       statuses: typing.Optional[Statuses] = None,
-                       date_start: typing.Optional['datetime.date'] = None,
-                       date_end: typing.Optional['datetime.date'] = None):
-        for result in self.csv_search(case_types=['R'],
-                                      statuses=statuses,
-                                      date_start=date_start,
-                                      date_end=date_end):
-            if result['case type'] == 'RC':
-                result.update(self.case_details(result['Case Number']))
-                yield result
+            date, document, party = row.xpath('./td')
 
-    def decertifications(self,
-                         statuses: typing.Optional[Statuses] = None,
-                         date_start: typing.Optional['datetime.date'] = None,
-                         date_end: typing.Optional['datetime.date'] = None):
-        for result in self.csv_search(case_types=['R'],
-                                      statuses=statuses,
-                                      date_start=date_start,
-                                      date_end=date_end):
-            if result['case type'] in {'RD', 'RM', 'UD'}:
-                result.update(self.case_details(result['Case Number']))
-                yield result
+            date_str = date.text.strip()
+            if date_str == 'pre 2010':
+                docket_entry['date'] = None
+            else:
+                docket_entry['date'] = datetime\
+                    .datetime\
+                    .strptime(date_str,
+                              '%m/%d/%Y').date()
+            if len(document):
+                document_link, = document.xpath('./a')
+                docket_entry['document'] = document_link.text.strip()
+                docket_entry['url'] = document_link.get('href')
+            else:
+                docket_entry['document'] = document.text.strip().strip('*')
 
-    def unit_clarifications(self,
-                            statuses: typing.Optional[Statuses] = None,
-                            date_start: typing.Optional['datetime.date'] = None,
-                            date_end: typing.Optional['datetime.date'] = None):
-        for result in self.csv_search(case_types=['R'],
-                                      statuses=statuses,
-                                      date_start=date_start,
-                                      date_end=date_end):
-            if result['case type'] == 'UC':
-                result.update(self.case_details(result['Case Number']))
-                yield result
+            docket_entry['issued_by/filed_by'] = party.text.strip() if party.text else None
 
-    def tallies(self):
-        '''https://www.nlrb.gov/reports/graphs-data/recent-election-results'''
-
-    def recent_filings(self):
-        '''https://www.nlrb.gov/reports/graphs-data/recent-filings?f[0]=case_type:R&date_start=05%2F01%2F2021&date_end=05%2F21%2F2021'''
-                              
+            yield docket_entry
 
 
 if __name__ == '__main__':
-    import datetime
-    import pprint
-
     s = NLRB()
-    s.cache_storage = scrapelib.FileCache('cache-directory')
-    s.cache_write_only = False
-    
-
-    # also 
-    pprint.pprint(s.case_details('14-RC-012769'))
-    #results = s.certifications(date_start=datetime.date(2021, 5, 1))
-    #for i, result in enumerate(results):
-    #    pprint.pprint(result)
-    #    print(i)
+    print(s.case_details('12-RC-008549'))
